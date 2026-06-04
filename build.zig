4a960bf194f826b088e90d697265a692d067c80c
cynumini <cynumini@proton.me>
2026-06-04 20:33:15 +0300

    decide to swtich to zig package management system

--- build.zig
+++ build.zig
@@ -21,13 +21,10 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
     const run_step = b.step("run", "Run omoshiroi");
     const run_cmd = b.addRunArtifact(exe);
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     run_step.dependOn(&run_cmd.step);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
